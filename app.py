fcb4f4f52352e0bd41bdd10ff22e1c86419530f3
Sunil071991 <90915202+Sunil071991@users.noreply.github.com>
2021-09-17 22:32:28 +0530

    aa

--- app.py
+++ app.py
@@ -1,44 +1,34 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 11 17:06:40 2021
-
-@author: Dippies
-"""
-import os
-import numpy as np
-from flask import Flask, request, jsonify, render_template, url_for
-import joblib
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from pycaret.regression import *
+import pandas as pd
 import pickle
+import numpy as np
 
-#os.chdir('/users/Dippies/Downloads')
-#print(os.getcwd())
+app = Flask(__name__)
 
-app = Flask(__name__, template_folder='template')
-#model = pickle.load(open('model.pkl', 'rb'))
-model = joblib.load('linreg.pkl')
-# takes 3 params
+model = load_model('deployment_28042020')
+cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
 
 @app.route('/')
 def home():
-	return render_template('home.html')
-
+    return render_template("home.html")
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        message = request.form['message']
-        data = [message]
-        print(message)
-        print(data)
-#        vect = cv.transform(data).toarray()
-        my_prediction = model.predict(data)
-        return render_template('prediction_outcome.html', prediction = my_prediction)
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data=data_unseen, round = 0)
+    prediction = int(prediction.Label[0])
+    return render_template('home.html',pred='Expected Bill will be {}'.format(prediction))
 
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = predict_model(model, data=data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
 
-# =============================================================================
-
-if __name__ == "__main__":
-    app.run(debug=False)
-    
-    
+if __name__ == '__main__':
+    app.run(debug=True)
